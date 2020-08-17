df163dbd59918b67a041855a52a498d3a0489011
olivier-belleval <belleval.olivier@gmail.com>
2020-08-17 21:16:59 +0200

    syntax change for query

--- Back/dataMappers/kanbanDataMapper.js
+++ Back/dataMappers/kanbanDataMapper.js
@@ -6,7 +6,12 @@ module.exports = {
 
     createKanban: async (kanbanObject) => {
 
-        const result = await client.query(`INSERT INTO "kanban"."kanban" ("title", "slug", "description", "background", "teacher_id") VALUES ($1, $2, $3, $4, $5) returning *`, [kanbanObject.title, kanbanObject.slug, kanbanObject.description, kanbanObject.background, kanbanObject.teacher_id]);
+        const query = {
+            text : `INSERT INTO "kanban"."kanban" ("title", "slug", "description", "background", "teacher_id") VALUES ($1, $2, $3, $4, $5) returning *`,
+            values: [kanbanObject.title, kanbanObject.slug, kanbanObject.description, kanbanObject.background, kanbanObject.teacher_id]
+          };
+
+        const result = await client.query(query);
 
         if(!result) {
             console.log('probleme a l\'insert');
