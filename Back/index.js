45f69f6669df18e2d2df5b5d4ecde829c67bf9d3
FlorianLevsr <florianlvsr@gmail.com>
2020-08-22 14:42:57 +0200

    added cors and body parser modules

--- Back/index.js
+++ Back/index.js
@@ -1,15 +1,26 @@
 const express = require('express');
 const app = express();
 
-const cors = require('cors')
+const cors = require('cors');
+const bodyParser = require('body-parser');
 
 require('dotenv').config();
 
 const router = require('./router');
 const session = require('express-session');
 
+app.use((request, response, next) => {
+    response.header('Access-Control-Allow-Origin', 'http://localhost:8080');
+    response.header('Access-Control-Allow-Credentials', true);
+    response.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    response.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+});
+
 // gestion du request.body
 app.use(express.urlencoded({extended: true}));
+
+app.use(bodyParser.json());
 
 // gestion session
 app.use(session({
@@ -19,9 +30,6 @@ app.use(session({
     saveUninitialized: true,
 
 }));
-
-// gestion CORS
-app.options('*',cors);
 
 app.use(router);
 
