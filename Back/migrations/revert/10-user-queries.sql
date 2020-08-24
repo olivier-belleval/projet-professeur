7f8fc3d9969808145265f015ab336c9fefcc90ab
FlorianLevsr <florianlvsr@gmail.com>
2020-08-24 09:58:55 +0200

    fix class_usernames_type in migration

--- Back/migrations/revert/10-user-queries.sql
+++ Back/migrations/revert/10-user-queries.sql
@@ -8,6 +8,7 @@ DROP FUNCTION get_class_by_username (TEXT);
 DROP FUNCTION get_teacher_by_username (TEXT);
 
 DROP FUNCTION get_all_classes_usernames();
-DROP TYPE class_usernames;
+
+DROP TYPE class_usernames_type;
 
 COMMIT;
