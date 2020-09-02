695ce30b078dedb1c26ccd9ce0a1f8a27d8c36f9
EmilieDevaux <hemery.emilie@gmail.com>
2020-09-02 08:43:23 +0200

    Bugfix SCSS

--- Front/src/components/AdminClass/index.js
+++ Front/src/components/AdminClass/index.js
@@ -1,6 +1,4 @@
 import React, { useEffect } from 'react';
-import { Dropdown } from 'semantic-ui-react';
-import 'semantic-ui-css/semantic.min.css';
 
 import { Link } from 'react-router-dom';
 
--- Front/src/components/ArticleDetail/index.js
+++ Front/src/components/ArticleDetail/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './style.scss';
 import { Link } from 'react-router-dom';
+import DOMPurify from "dompurify";
 
 const ArticleDetail = ({ article }) => {
   console.log(article);
@@ -13,12 +14,12 @@ const ArticleDetail = ({ article }) => {
           {article.article_title}
         </h2>
         <p className="article-detail-head-subtitle">
-          Rédigé {article.article_author} le 36 janvier 1478
         </p>
       </header>
       <main className="article-detail-content">
-        <p>
-            {article.article_excerpt}
+        <p
+        dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(article.article_content) }}>
+
         </p>
       </main>
       <footer>
