5e67703d1cd1bf1c096d8fa4d52147885c095b02
dimitri-hoareau <dimitrihoareau2@gmail.com>
2020-09-03 11:35:10 +0200

    add 404 with responsive content

---
+++ Front/src/components/NotFound/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './style.scss';
+
+const NotFound = () => (
+  <div className="not-found">
+    <h2>
+      404
+    </h2>
+    <p>Désolé, cette page n'existe pas</p>
+
+    <img src="https://c4.wallpaperflare.com/wallpaper/432/592/854/website-link-zelda-the-legend-of-zelda-wallpaper-preview.jpg" alt="image_de_link_mort" />
+
+  </div>
+);
+export default NotFound;
